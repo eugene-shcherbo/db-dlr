4bf8d35bfdb3291820757b08ba5d740bbe55471d
Eugene Shcherbo <eugene.shcherbo@gmail.com>
2020-09-09 22:20:54 +0300

    initial commit + connecting to db

---
+++ dynamic-database/Database.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Dynamic;
+using System.Data.SqlClient;
+
+namespace dynamic_database
+{
+    public class Database : DynamicObject
+    {
+        private readonly string connectionString;
+
+        public Database(string connectionString)
+        {
+            this.connectionString = connectionString;
+            TestConnection();
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            Console.WriteLine("member invoked");
+            result = null;
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            Console.WriteLine("member set");
+            return true;
+        }
+
+        private void TestConnection()
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                Console.WriteLine("Connected successfully.");
+
+                Console.WriteLine("Press any key to finish...");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
---
+++ dynamic-database/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace dynamic_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfigurationRoot config = CreateConfig();
+            var connectioString = config.GetValue<string>("ConnectionString");
+
+            if (string.IsNullOrWhiteSpace(connectioString))
+            {
+                Console.WriteLine("Please provide a connection string");
+                return;
+            }
+
+            dynamic database = new Database(connectioString);
+            Action helloWorld = () => { Console.WriteLine("Hello World"); };
+            database.HelloWorld = helloWorld;
+            database.HelloWorld();
+        }
+
+        static IConfigurationRoot CreateConfig()
+        {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+            return builder.Build();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets<Program>();
+        }
+    }
+}
